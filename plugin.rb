b5c5e0b036ec3e2d49300a3c60e8ab0ffa9e5853
Angus McLeod <angus@mcleod.org.au>
2020-05-27 12:21:57 +1000

    Ensure agenda and calendar are valid top_menu choices

--- plugin.rb
+++ plugin.rb
@@ -66,11 +66,18 @@ after_initialize do
 
   module EventsSiteSettingExtension
     def type_hash(name)
-      if name == :top_menu
-        @choices[name].push("agenda") if @choices[name].exclude?("agenda")
-        @choices[name].push("calendar") if @choices[name].exclude?("calendar")
-      end
-      super(name)
+      add_choices(name) if name == :top_menu
+      super
+    end
+    
+    def validate_value(name, type, val)
+      add_choices(name) if name == :top_menu
+      super
+    end
+    
+    def add_choices(name)
+      @choices[name].push("agenda") if @choices[name].exclude?("agenda")
+      @choices[name].push("calendar") if @choices[name].exclude?("calendar")
     end
   end
 
