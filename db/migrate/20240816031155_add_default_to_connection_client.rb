8913094cf6a5edf01f2e8ad99e464fc8db0413a8
Angus McLeod <angus@mcleod.org.au>
2024-08-16 11:27:49 +0800

    Fix stree

--- db/migrate/20240816031155_add_default_to_connection_client.rb
+++ db/migrate/20240816031155_add_default_to_connection_client.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 class AddDefaultToConnectionClient < ActiveRecord::Migration[7.1]
   def change
-    change_column_default :discourse_events_connections, :client, 'events'
+    change_column_default :discourse_events_connections, :client, "events"
   end
 end
