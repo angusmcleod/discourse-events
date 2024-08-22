7760067d7b031ffaa64e2928078e218a59f8e435
Angus McLeod <angus@mcleod.org.au>
2024-08-22 15:19:01 +0800

    Bump version

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 # name: discourse-events
 # about: Allows you to manage events in Discourse
-# version: 0.6.2
+# version: 0.7.0
 # authors: Angus McLeod
 # contact_emails: angus@pavilion.tech
 # url: https://github.com/paviliondev/discourse-events
