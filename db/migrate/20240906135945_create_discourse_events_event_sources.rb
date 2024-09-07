5b794b14a128f9f365d86cd486bfca19aca22dc3
Angus McLeod <angus@mcleod.org.au>
2024-09-07 10:53:28 +0200

    Add index handling to migration

--- db/migrate/20240906135945_create_discourse_events_event_sources.rb
+++ db/migrate/20240906135945_create_discourse_events_event_sources.rb
@@ -15,9 +15,25 @@ class CreateDiscourseEventsEventSources < ActiveRecord::Migration[7.1]
              WHERE events.source_id IS NOT NULL"
 
     change_column_null :discourse_events_events, :uid, true
+    remove_index :discourse_events_events, name: "discourse_events_event_id_index"
+    remove_index :discourse_events_events, name: "index_discourse_events_events_on_provider_id"
+    remove_index :discourse_events_events, name: "index_discourse_events_events_on_source_id"
   end
 
   def down
     change_column_null :discourse_events_events, :uid, false
+    add_index :discourse_events_events,
+              %i[uid provider_id],
+              unique: true,
+              name: "discourse_events_event_id_index",
+              if_not_exists: true
+    add_index :discourse_events_events,
+              %i[provider_id],
+              name: "index_discourse_events_events_on_provider_id",
+              if_not_exists: true
+    add_index :discourse_events_events,
+              %i[source_id],
+              name: "index_discourse_events_events_on_source_id",
+              if_not_exists: true
   end
 end
