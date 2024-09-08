de3a3d155d1673da17745612c38a78ec4a053219
merefield <merefield@gmail.com>
2024-09-08 06:56:20 -0400

    add deadline attribute migration

---
+++ db/migrate/20240905012439_add_deadline_to_discourse_events_events.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+class AddDeadlineToDiscourseEventsEvents  < ActiveRecord::Migration[7.1]
+  def change
+    add_column :discourse_events_events, :deadline, :boolean, default: false
+  end
+end
