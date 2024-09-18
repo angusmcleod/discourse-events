7b0049825ccb0de3fa42cec8dd2e87713da0b508
merefield <merefield@gmail.com>
2024-09-18 15:19:15 +0100

    delete unnecessary migration

--- db/migrate/20240905012439_add_deadline_to_discourse_events_events.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-class AddDeadlineToDiscourseEventsEvents < ActiveRecord::Migration[7.1]
-  def change
-    add_column :discourse_events_events, :deadline, :boolean, default: false
-  end
-end
